7c42bf9db613cbe1406db9ab206d9963ef479aae
Zach Holcomb <zachholcombmusic@gmail.com>
2020-07-20 11:03:33 -0600

    Restructure files

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://user@postgresserver/db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres@localhost"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
