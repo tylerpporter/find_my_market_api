2c14b3fb50fd07b3432b476930dc6e959b79709e
Tyler Porter <tylerpporter@gmail.com>
2020-07-24 10:27:50 -0600

    Test teardown working

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,6 +2,7 @@ from IPython import embed
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session
 from app.database import Base
 from app.main import app, get_db
 import pytest
@@ -14,29 +15,31 @@ from app import crud
 from app.schemas import UserCreate
 from os import getenv
 engine = create_engine(getenv("DATABASE_URL"))
-# TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
 
-@pytest.fixture(scope="session")
-def db() -> Generator:
-    yield SessionLocal()
-#
-# @pytest.fixture(scope="session", autouse=True)
-# def clean_up():
-#     Base.metadata.create_all(bind=engine)
-#     yield
-#     Base.metadata.drop_all(bind=engine)
-
-def test_create_user(db: Session):
+def test_create_user():
+    db = SessionLocal()
     email = "dan@example.com"
     user_in = UserCreate(email=email)
     user = crud.create_user(db, user=user_in)
     assert user.email == email
+    engine.dispose()
+    db.rollback()
+    db.close()
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
 
-def test_create_user_again(db: Session):
+
+def test_create_user_again():
+    db = SessionLocal()
     email = "dan@example.com"
     user_in = UserCreate(email=email)
     user = crud.create_user(db, user=user_in)
     assert user.email == email
+    engine.dispose()
+    db.rollback()
+    db.close()
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
