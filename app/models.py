643b4f83a3f396657c02d7554995ce713f3d382a
Zach Holcomb <zachholcombmusic@gmail.com>
2020-07-24 12:28:46 -0600

    Add favorites to models and add many to many

--- app/models.py
+++ app/models.py
@@ -6,24 +6,24 @@ class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
-    # favorites = relationship(
-    #     "Market",
-    #     secondary="favorites",
-    #     backref="users"
-    # )
+    favorites = relationship(
+        "Market",
+        secondary="favorites",
+        backref="users"
+    )
 
 class Market(Base):
     __tablename__ = "markets"
     id = Column(Integer, primary_key=True, index=True)
     market_id = Column(Integer, index=True)
-    # favorites = relationship(
-    #     "User",
-    #     secondary="favorites",
-    #     backref="markets"
-    # )
+    favorites = relationship(
+        "User",
+        secondary="favorites",
+        backref="markets"
+    )
 
-# class Favorite(Base):
-#     __tablename__ = "favorites"
-#     id = Column(Integer, primary_key=True, index=True)
-#     market_id = Column(Integer, foreign_key=True)
-#     user_id = Column(Integer, foreign_key=True)
+class Favorite(Base):
+    __tablename__ = "favorites"
+    id = Column(Integer, primary_key=True, index=True)
+    market_id = Column(Integer, ForeignKey("markets.id"))
+    user_id = Column(Integer, ForeignKey("users.id"))
