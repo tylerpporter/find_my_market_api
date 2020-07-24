db87e5f77b5aea8ac4c8c7fc1367cc1c545b80e3
Zach Holcomb <zachholcombmusic@gmail.com>
2020-07-24 14:20:54 -0600

    Add multiple favorites to favorites test

--- tests/models/test_favorite.py
+++ tests/models/test_favorite.py
@@ -28,14 +28,22 @@ def cleanup():
 def test_create_user_favorites(db, cleanup):
   email = "bob@example.com"
   market_id = 1
+  market_id2 = 2
   user_in = UserCreate(email=email)
   market_in = MarketCreate(market_id=market_id)
+  market_in2 = MarketCreate(market_id=market_id2)
   user = crud.create_user(db, user=user_in)
-  market = crud.create_market(db, market= market_in)
+  market = crud.create_market(db, market=market_in)
+  market2 = crud.create_market(db, market=market_in2)
   favorite_in = FavoriteCreate(user_id=user.id, market_id=market.id)
+  favorite_in2 = FavoriteCreate(user_id=user.id, market_id=market2.id)
   favorite = crud.favorite_market(db, favorite=favorite_in)
+  favorite2 = crud.favorite_market(db, favorite=favorite_in2)
 
   assert favorite.user_id == user.id
   assert favorite.market_id == market.id
+  assert len(user.favorites) == 2
   assert user.favorites[0] == market
+  assert user.favorites[1] == market2
+  
   
