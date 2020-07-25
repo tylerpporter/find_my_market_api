8a475cb0b93c9f5bd75fc2fac9da173406b4a406
Colin Alexander <colintalex@gmail.com>
2020-07-25 12:09:38 -0600

    Writes test for multiple user requests

---
+++ tests/requests/test_requests.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+
+from IPython import embed
+from sqlalchemy import create_engine
+from app.database import Base
+from app.main import app
+import pytest
+from fastapi.encoders import jsonable_encoder
+from app.database import SessionLocal
+from app import crud
+from app.schemas import UserCreate
+from os import getenv
+
+engine = create_engine(getenv("DATABASE_URL"))
+Base.metadata.drop_all(bind=engine)
+Base.metadata.create_all(bind=engine)
+
+@pytest.fixture
+def db():
+    Base.metadata.create_all(bind=engine)
+    db = SessionLocal()
+    yield db
+    db.close()
+
+@pytest.fixture
+def cleanup():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+
+client = TestClient(app)
+
+def test_can_get_all_users(db, cleanup):
+    email = "dan@example.com"
+    user_in = UserCreate(email=email)
+    user = crud.create_user(db, user=user_in)
+    email2 = "bob@example.com"
+    user_in2 = UserCreate(email=email2)
+    user2 = crud.create_user(db, user=user_in2)
+
+    response = client.get("/users/")
+    assert response.status_code == 200
+    resp = response.json()
+    assert resp == [{'email': 'dan@example.com', 'id': 1},
+                    {'email': 'bob@example.com', 'id': 2}]
+    assert len(resp) == 2
