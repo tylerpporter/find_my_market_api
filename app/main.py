ef5c6e031153ecf952ea6ac70fd8cef5e3fcac2e
Colin Alexander <colintalex@gmail.com>
2020-07-25 14:57:26 -0600

    Refactors all routes except create user until OAuth is implemented

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ from app.api.routes import api_router
 app = FastAPI()
 app.include_router(api_router)
 
+#  WAITING FOR O-AUTH TO IMPLEMENT CREATE USER
 # @app.post("/users/", response_model=schemas.User)
 # def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 #     db_user = crud.get_user_by_email(db, email=user.email)
