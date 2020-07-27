1cfe404120d15dd1b21e8de62111f0196e4f6ecd
Zach Holcomb <zachholcombmusic@gmail.com>
2020-07-26 21:29:19 -0600

    Add method to get current user in deps

--- app/api/deps.py
+++ app/api/deps.py
@@ -1,5 +1,16 @@
 from app.database import SessionLocal
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt
+from pydantic import ValidationError
+from sqlalchemy.orm import Session
+from app import crud, models, schemas
+from app.security import ALGORITHM
+from app.config import settings
 
+reusable_oauth2 = OAuth2PasswordBearer(
+    tokenUrl="/login/token"
+)
 
 def get_db():
     db = SessionLocal()
@@ -7,3 +18,21 @@ def get_db():
         yield db
     finally:
         db.close()
+
+def get_current_user(
+    db: Session = Depends(get_db), token: str = Depends(reusable_oauth2)
+) -> models.User:
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[ALGORITHM]
+        )
+        token_data = schemas.TokenPayload(**payload)
+    except (jwt.JWTError, ValidationError):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Could not validate credentials"
+        )
+    user = crud.get_user(db, user_id=token_data.sub)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
