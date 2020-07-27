be236a85e00b67dfb4e5d0af4573d9200cb52ba7
Zach Holcomb <zachholcombmusic@gmail.com>
2020-07-26 21:29:31 -0600

    Add authenticate user to crud

--- app/crud.py
+++ app/crud.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
 from . import models, schemas
+from app.security import get_hashed_password, verify_password
 
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
@@ -10,8 +11,19 @@ def get_user_by_email(db: Session, email: str):
 def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
 
+def authenticate_user(db: Session, email: str, password: str):
+    user = get_user_by_email(db, email=email)
+    if not user:
+        return None
+    if not verify_password(password, user.hashed_password):
+        return None
+    return user
+
 def create_user(db: Session, user: schemas.UserCreate):
-    db_user = models.User(email=user.email)
+    db_user = models.User(
+        email=user.email,
+        hashed_password=get_hashed_password(user.password)
+        )
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
