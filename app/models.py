4b9c027b67d21e063a324724f73a7aa49455bd14
Zach Holcomb <zachholcombmusic@gmail.com>
2020-07-26 21:30:01 -0600

    Add password to user model

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
+    hashed_password = Column(String, nullable=False)
     favorites = relationship(
         "Market",
         secondary="favorites",
