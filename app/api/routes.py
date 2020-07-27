51b760a24a7b546014ae0136f8e06fd6b0b113f0
Zach Holcomb <zachholcombmusic@gmail.com>
2020-07-26 21:28:43 -0600

    Add login to router

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from app.api.endpoints import users, markets
+from app.api.endpoints import users, markets, login
 
 api_router = APIRouter()
 api_router.include_router(users.router, prefix="/users")
 api_router.include_router(markets.router, prefix="/markets")
+api_router.include_router(login.router)
