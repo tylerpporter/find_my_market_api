7f994f842f7187087ae7dffeb98697af6b5ed7f1
Zach Holcomb <zachholcombmusic@gmail.com>
2020-07-30 20:35:36 -0600

    Add prelim test for favorite delete request

--- tests/requests/test_favorites_requests.py
+++ tests/requests/test_favorites_requests.py
@@ -69,3 +69,24 @@ def test_it_can_get_all_user_favorites(db, cleanup):
     assert response.status_code == 200
     resp = response.json()
     assert resp['favorites'] == [{'market_id': 123456, 'id': 1}]
+
+def test_it_can_delete_user_favorites(db, cleanup):
+    market_id = 123456
+    market_in = MarketCreate(market_id=market_id)
+    market = crud.create_market(db, market=market_in)
+    email = "dan@example.com"
+    password = "123456"
+    user_in = UserCreate(email=email, password=password)
+    user = crud.create_user(db, user=user_in)
+    client.post(f"/users/{user.id}/favorites",
+    json={"fmid": market_id})
+    response = client.get(f"/users/{user.id}")
+    user_resp = response.json()
+
+    assert len(user_resp['favorites']) == 1
+
+    response2 = client.delete(f"/users/{user.id}/favorites",
+    json={"fmid": market_id})
+    user_resp2 = response2.json()
+
+    assert len(user_resp2['favorites']) == 0
