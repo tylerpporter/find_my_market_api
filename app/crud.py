4634f14c59e4294f7afe7fd58311d52a3fc3bef6
Zach Holcomb <zachholcombmusic@gmail.com>
2020-07-30 20:35:54 -0600

    Add favorite delete method to crud

--- app/crud.py
+++ app/crud.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Session
 from . import models, schemas
 from app.security import get_hashed_password, verify_password
+from IPython import embed
 
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
@@ -48,3 +49,11 @@ def favorite_market(db: Session, favorite: schemas.FavoriteCreate):
     db.commit()
     db.refresh(db_favorite)
     return db_favorite
+
+def delete_user_favorite(db: Session, user_id: int, market_id: int):
+    db_market = db.query(models.Market).filter(models.Market.market_id == market_id).first().id
+    db_fav = db.query(models.Favorite).filter(models.Favorite.user_id == user_id).filter(models.Favorite.market_id == db_market).first()
+    db.delete(db_fav)
+    db.commit()
+    user = get_user(db, user_id=user_id)
+    return user
