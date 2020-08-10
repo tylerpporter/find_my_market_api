e2f2513ed45dbb6fabd50a080c66003b7574bafd
Zach Holcomb <zachholcombmusic@gmail.com>
2020-08-10 11:16:31 -0600

    Updates user model and responses

--- app/models/user.py
+++ app/models/user.py
@@ -7,6 +7,8 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String, nullable=False)
+    username = Column(String, nullable=True)
+    image = Column(String, nullable=True)
     favorites = relationship(
         "Market",
         secondary="favorites",
--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 from pydantic import BaseModel
 from pydantic import EmailStr
 from .market import Market
@@ -9,9 +9,17 @@ class UserBase(BaseModel):
 class User(UserBase):
     id: int
     favorites: List[Market] = []
+    image: Optional[str]
+    username: Optional[str]
     class Config:
         orm_mode = True
 
 class UserCreate(UserBase):
     email: EmailStr
     password: str
+    image: Optional[str]
+    username: Optional[str]
+
+# class UserUpdate(UserBase):
+#     email: EmailStr
+    
