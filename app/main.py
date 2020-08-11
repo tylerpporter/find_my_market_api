2ba15c7b2f7e1ed4bd10baf9db0954ea2c55761b
Zach Holcomb <zachholcombmusic@gmail.com>
2020-08-10 18:11:50 -0600

    Adds base to models init

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 # from typing import List
 from fastapi import FastAPI
 from . import crud, models, schemas
-from .database import SessionLocal, engine, Base
+from .database import SessionLocal, engine
 from app.api.routes import api_router
 models.Base.metadata.create_all(bind=engine)
 
