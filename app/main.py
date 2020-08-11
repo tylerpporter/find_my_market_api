518d3e9a77aa9b6e4381fd3256aa6620e5af90af
Zach Holcomb <zachholcombmusic@gmail.com>
2020-08-10 18:09:26 -0600

    Add import base to main

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 # from typing import List
 from fastapi import FastAPI
 from . import crud, models, schemas
-from .database import SessionLocal, engine
+from .database import SessionLocal, engine, Base
 from app.api.routes import api_router
 models.Base.metadata.create_all(bind=engine)
 
