deaba87dfe502b23e1a488dc992425b9802d8416
Zach Holcomb <zachholcombmusic@gmail.com>
2020-08-10 18:03:44 -0600

    Update main for heroku

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from . import crud, models, schemas
 from .database import SessionLocal, engine
 from app.api.routes import api_router
-# models.Base.metadata.create_all(bind=engine)
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.include_router(api_router)
